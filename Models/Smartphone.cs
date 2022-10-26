c429b776c1a0f0e8d2757f93886068f0a993977e
lelebertozzi@hotmail.com <lelebertozzi@hotmail.com>
2022-10-26 01:28:12 -0300

    Desafio Concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,18 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
+        // IMPLEMENTADO
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            // IMPLEMENTADO
         }
 
         public void Ligar()
@@ -19,6 +26,12 @@ namespace DesafioPOO.Models
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+        
+        public void Configuracoes()
+        {
+            Console.WriteLine($"O numero deste telefone eh {Numero}, modelo {Modelo}, IMEI {IMEI} e tem {Memoria} GB de memoria");
+
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "989234", modelo: "Azul", imei: "123456", memoria: 5);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Jogo da Cobrinha");
+nokia.Configuracoes();
+Console.WriteLine("\n");
+Smartphone iphone = new Iphone(numero: "876543", modelo: "Prata", imei: "654321", memoria: 25);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Safari");
+iphone.Configuracoes();
